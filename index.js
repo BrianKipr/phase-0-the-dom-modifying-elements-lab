0d28fb0c53f90fab2af7a1431f51402abc0260b5
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-06-20 08:16:12 +0300

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+function newHeader() {
+    const h1Node = document.createElement('h1');
+    h1Node.id = 'victory';
+    h1Node.innerHTML = 'victory is the champion'; // Replace YOUR-NAME with your actual name
+  
+    const mainElement = document.querySelector('main#main');
+    if (mainElement) {
+      mainElement.remove();
+    }
+  
+    document.body.appendChild(h1Node);
+  
+    const victory = h1Node.nodeName;
+    console.log(victory);
+  
+    const champion = h1Node.innerHTML;
+    console.log(champion);
+}
